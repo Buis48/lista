3553671899438d71690293f74b2017044fc0fafa
Luis C Mtz <lcmh.down.48@gmail.com>
2023-04-02 12:10:17 -0600

    Carga de index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es" xml:lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de asistencia 1° Jornada Académica de Residentes de Medicina Familiar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+</head>
+<body class="font-family">
+
+<div class="container-fluid p-5 px-1 text-center">
+<div class="container text-center">
+    
+<h1 class= "display-6">1° Jornada Académica de Residentes de Medicina Familiar</h1>
+<blockquote class="blockquote text-light-emphasis">
+    <p>Lista de asistencia.</p>
+  </blockquote>
+</div>
+
+
+
+<div>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</html>
