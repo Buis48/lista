e59ba67bc6f57d5cce932a174b9632149cc0968d
Luis Martinez <56004982+Buis48@users.noreply.github.com>
2023-04-13 13:47:27 -0600

    Actualizar solicitud.php

--- solicitud.php
+++ solicitud.php
@@ -8,6 +8,7 @@
     <title>Constancia 1° Jornada Académica de Residentes de Medicina Familiar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+
 </head>
 
 <body style="background-image: url('img/fondo.jpg');
